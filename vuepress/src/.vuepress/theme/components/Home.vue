614dab4f12e27663c8329d0b8f1b047c8c9fa12e
Long Nguyen <lnguyen@globalia.ca>
2021-01-05 10:12:19 -0500

    tumblelog

--- vuepress/src/.vuepress/theme/components/Home.vue
+++ vuepress/src/.vuepress/theme/components/Home.vue
@@ -9,13 +9,14 @@
           span.info-bubble
             span.info-bubble-symbol ?
             .info-bubble-details
-              p The idea was to create a section for micro-blogging for my website and I discovered these micro posts are called tumblelogs, since 2005.
+              p The idea was to create a section for micro-blogging and I discovered these micro posts are called tumblelogs, since 2005.
               blockquote
                 p A tumblelog is a quick and dirty stream of consciousness [...]. They remind me of an older style of blogging, back when people did sites by hand, [...] really just a way to quickly publish the "stuff" that you run across every day on the web.
                 p
                   a(target="_blank" href="https://kottke.org/05/10/tumblelogs")
                     strong
                       em Jason Kottke, October 19, 2005
+        Posts(type="tumblelogs")
         //- https://help.twitter.com/en/using-twitter/embed-twitter-feed
         //- https://publish.twitter.com/?lang=en&query=https%3A%2F%2Ftwitter.com%2Flongzero&theme=light&widget=Timeline
         //- https://developer.twitter.com/en/docs/twitter-for-websites/timelines/overview
@@ -29,8 +30,6 @@
         .twitter-widget
           a(class="twitter-timeline" data-tweet-limit="9" data-lang="en" data-theme="light" data-chrome="noheader transparent" href="https://twitter.com/Longzero?ref_src=twsrc%5Etfw")
           script(async src="https://platform.twitter.com/widgets.js" charset="utf-8")
-
-        //- Posts(type="tumblelog")
 </template>
 
 <script>
