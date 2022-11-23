fd7fabdb4adeeb90e76786858b7bf49f75b29839
Long Nguyen <lnguyen@globalia.ca>
2022-11-22 23:44:08 -0500

    setup: new 2022 folder

---
+++ 2022/assets/js/12.2dafab6f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{291:function(t,s,n){"use strict";n.r(s);var o=n(13),e=Object(o.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[this._v("Just testing what it's like to post from git.")])])}),[],!1,null,null,null);s.default=e.exports}}]);
