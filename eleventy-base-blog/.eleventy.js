6d0ab097429355c64f68e9e35a5959c90a9a5f75
Long Nguyen <lnguyen@globalia.ca>
2023-01-16 20:14:05 -0500

    search: functional, still need to style it

--- eleventy-base-blog/.eleventy.js
+++ eleventy-base-blog/.eleventy.js
@@ -7,6 +7,7 @@ const markdownItAnchor = require("markdown-it-anchor");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const searchFilter = require("./filters/searchFilter");
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
@@ -102,6 +103,9 @@ module.exports = function(eleventyConfig) {
         .getFilteredByGlob("./posts/*.md") // [3]
         .filter(publishedPosts); // [4]
   });
+
+  // https://www.belter.io/eleventy-search/
+  eleventyConfig.addFilter("search", searchFilter);
 
   return {
     // Control which files Eleventy will process
--- eleventy-base-blog/assets/js/main.js
+++ eleventy-base-blog/assets/js/main.js
@@ -44,3 +44,48 @@ document.querySelector('.js-toggle-dark-light-mode').addEventListener('click', f
   else colorScheme = "dark"
   setCookie("color_scheme", colorScheme, 365);
 })
+
+
+
+// SEARCH
+// https://www.belter.io/eleventy-search/
+const search = (e) => {
+  const results = window.searchIndex.search(e.target.value, {
+    bool: "OR",
+    expand: true,
+  });
+
+  const resEl = document.getElementById("searchResults");
+  const noResultsEl = document.getElementById("noResultsFound");
+
+  resEl.innerHTML = "";
+  if (results) {
+    noResultsEl.style.display = "none";
+    results.map((r) => {
+      const { id, title, description } = r.doc;
+      const el = document.createElement("li");
+      resEl.appendChild(el);
+
+      const h3 = document.createElement("h3");
+      el.appendChild(h3);
+
+      const a = document.createElement("a");
+      a.setAttribute("href", id);
+      a.textContent = title;
+      h3.appendChild(a);
+
+      const p = document.createElement("p");
+      p.textContent = description;
+      el.appendChild(p);
+    });
+  } else {
+    noResultsEl.style.display = "block";
+  }
+};
+
+fetch("/search-index.json").then((response) =>
+  response.json().then((rawIndex) => {
+    window.searchIndex = elasticlunr.Index.load(rawIndex);
+    document.getElementById("searchField").addEventListener("input", search);
+  })
+);
