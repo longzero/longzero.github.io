8412babbeb7dc1e23f583141b67666c2baf710d9
Long Nguyen <long@longzero.com>
2023-12-29 12:13:00 -0700

    map: add marker only after user trigger (in progress)

--- eleventy-base-blog/assets/js/map/map.js
+++ eleventy-base-blog/assets/js/map/map.js
@@ -1,7 +1,32 @@
+DEBUG = true
+
+
 // TOGGLE LEGEND
 // document.querySelector('.js-legend-toggle').addEventListener('click', function() {
 //   this.parentNode.classList.toggle('minimize')
 // })
+let legendClass = 'map-legend-item'
+let legendHtml = '' // Create each item of legend, in HTML
+let legendMarker = ''
+let locationTypeHuman = '' // Used for labelling the legend, so instead of just “Water”, it will be “Place to get water”
+
+let map = L.map('map', {
+  maxZoom: 20,
+  // https://github.com/mutsuyuki/Leaflet.SmoothWheelZoom
+  scrollWheelZoom: false, // disable original zoom function
+  smoothWheelZoom: true,  // enable smooth zoom
+  smoothSensitivity: 8,   // zoom speed. default is 1
+  // END https://github.com/mutsuyuki/Leaflet.SmoothWheelZoom
+}).fitBounds([
+  [54.14584949174648, -128.75881463815347],
+  [17.055714221336178, -59.9846634648619]
+])
+// Request permission to get location.
+// .locate({
+//   setView: false, // true means the map zooms to current location.
+//   maxZoom: 8
+// })
+
 
 
 // MAP MARKER ICONS
@@ -90,8 +115,165 @@ const svgWater = L.icon({
 // END MAP MARKER ICONS
 
 
+
+function addMarkers(count, locations, locationType) {
+  // LOOP THROUGH LOCATIONS AND ADD THEM TO THE MAP.
+  for (let locationItem in locations[locationType]) {
+    // DEBUG && console.log(locationType)
+    // DEBUG && console.log(locationItem)
+
+    let location = locations[locationType][locationItem]
+
+    // Check if location should be on the map and obviously, make sure there are coordinates.
+    if (location.visible !== 0 && location.latitude && location.longitude) {
+
+      count++
+
+      let locationName = "",
+          locationNotes = "",
+          locationUrl = ""
+
+      // Create URL to Google Maps from coordinates.
+      let googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=" + location.latitude + "%2C" + location.longitude
+
+      // If there is no name, create it from coordinates.
+      if (location.name == undefined) locationName = location.latitude + ", " + location.longitude
+      else locationName = location.name
+      locationName = '<a class="location-tooltip-name" href="' + googleMapsUrl + '" target="_blank">' + locationName + '</a>'
+
+      // Check other content
+      if (location.notes !== undefined && location.notes !== "") locationNotes = '<div class="location-tooltip-notes">' + location.notes + '</div>'
+      if (location.url !== undefined && location.url !== "") locationUrl = '<a class="location-tooltip-action" target="_blank" href="' + location.url + '">' + location.url + '</a>'
+
+      // Build the content of tooltips.
+      let locationContent = '<div class="location-tooltip-content">' + locationName + locationNotes + locationUrl + '</div>'
+
+      // Place markers on the map.
+      marker = new L.marker([location.latitude, location.longitude], {icon: svgIcon})
+        .bindPopup(locationContent, { offset: L.point(0,-14) })
+        // Comment this line if using clusters
+        .addTo(map);
+
+      // Uncomment this line if using clusters
+      // markers.addLayer(marker);
+    } // if
+  } // for (individual places)
+
+  // End legend item with the label.
+  // legendHtml += '" alt=""></div><div class="map-legend-label">'+ locationTypeHuman +' ('+ locations[locationType].length +')</div></li>'
+  legendHtml += '" alt=""></div><div class="map-legend-label '+ locations[locationType].length +'">'+ locationTypeHuman +' ('+ count +')</div></li>'
+}
+
+function parseLocations(locations) {
+  for (let locationType in locations) {
+    // Change marker icon depending on type of location.
+    // Set the marker path for the legend item icon.
+    switch(locationType) {
+      case "blm":
+        // Use the class hide to hide by default (use that string to search elsewhere it is used)
+        legendClass = 'map-legend-item js-map-legend-item blm hide ' + locationType
+        // legendHtml += markerPathBLM
+        legendMarker = markerPathBLM
+        locationTypeHuman = 'Some BLM'
+        svgIcon = svgBLM
+        break
+      case "places":
+        legendClass = 'map-legend-item js-map-legend-item ' + locationType
+        // legendHtml += markerPathPlace
+        legendMarker = markerPathPlace
+        locationTypeHuman = 'Random place'
+        svgIcon = svgPlace
+        break
+      case "potential-spots":
+        // Use the class hide to hide by default (use that string to search elsewhere it is used)
+        legendClass = 'map-legend-item js-map-legend-item hide ' + locationType
+        legendMarker = markerPathPotential
+        locationTypeHuman = 'Potential spots'
+        svgIcon = svgPotential
+        break
+      case "spots":
+        legendClass = 'map-legend-item js-map-legend-item ' + locationType
+        // legendHtml += markerPathCampspot
+        legendMarker = markerPathCampspot
+        locationTypeHuman = 'Chilling, sleeping and/or working spot'
+        svgIcon = svgSpots
+        break
+      case "starbucks":
+        legendClass = 'map-legend-item js-map-legend-item ' + locationType
+        // legendHtml += markerPathPlace
+        legendMarker = markerPathPlace
+        locationTypeHuman = 'Starbucks'
+        svgIcon = svgStarbucks
+        break
+      case "shower":
+        legendClass = 'map-legend-item js-map-legend-item ' + locationType
+        // legendHtml += markerPathWater
+        legendMarker = markerPathWater
+        locationTypeHuman = 'Shower spot'
+        svgIcon = svgShower
+        break
+      case "water":
+        legendClass = 'map-legend-item js-map-legend-item ' + locationType
+        // legendHtml += markerPathWater
+        legendMarker = markerPathWater
+        locationTypeHuman = 'Water for cleaning or drinking'
+        svgIcon = svgWater
+        break
+      default:
+        legendClass = 'map-legend-item js-map-legend-item ' + locationType
+        // legendHtml += markerPathCampspot
+        legendMarker = markerPathCampspot
+        svgIcon = svgDefault
+    } // switch
+
+
+    legendHtml += '<li class="'+ legendClass +'"><div class="map-legend-symbol"><img src="' + legendMarker
+
+    let count = 0 // For counting locations by type.
+    addMarkers(count, locations, locationType)
+  } // for (first level in json)
+}
+
+function toggleLegendAndMarkers() {
+  // TOGGLE LEGEND AND MARKERS
+  const legendToggle = document.querySelectorAll('.js-map-legend-item')
+  for (let i = 0; i < legendToggle.length; i++) {
+    legendToggle[i].addEventListener('click', function() {
+      this.classList.toggle('hide')
+      let markers
+      if (this.classList.contains('current-location')) {
+        markers = document.querySelectorAll('.js-marker-icon.current-location')
+      }
+      else if (this.classList.contains('spots')) {
+        markers = document.querySelectorAll('.js-marker-icon.spots')
+      }
+      else if (this.classList.contains('blm')) {
+        markers = document.querySelectorAll('.js-marker-icon.blm')
+      }
+      else if (this.classList.contains('places')) {
+        markers = document.querySelectorAll('.js-marker-icon.places')
+      }
+      else if (this.classList.contains('potential-spots')) {
+        markers = document.querySelectorAll('.js-marker-icon.potential-spots')
+      }
+      else if (this.classList.contains('shower')) {
+        markers = document.querySelectorAll('.js-marker-icon.shower')
+      }
+      else if (this.classList.contains('starbucks')) {
+        markers = document.querySelectorAll('.js-marker-icon.starbucks')
+      }
+      else if (this.classList.contains('water')) {
+        markers = document.querySelectorAll('.js-marker-icon.water')
+      }
+      for (let j = 0; j < markers.length; j++) {
+        markers[j].classList.toggle('hide')
+      }
+    })
+  }
+}
+
 function initMap(locations) {
-  // console.log(locations)
+  // DEBUG && console.log(locations)
 
   // This is the code to start the map at a specific location.
   // var map = L.map('map').setView([38.973268454455706, -105.03998309979006], 14);
@@ -137,24 +319,6 @@ function initMap(locations) {
   // });
 
 
-  var map = L.map('map', {
-    maxZoom: 20,
-    // https://github.com/mutsuyuki/Leaflet.SmoothWheelZoom
-    scrollWheelZoom: false, // disable original zoom function
-    smoothWheelZoom: true,  // enable smooth zoom
-    smoothSensitivity: 8,   // zoom speed. default is 1
-    // END https://github.com/mutsuyuki/Leaflet.SmoothWheelZoom
-  }).fitBounds([
-    [54.14584949174648, -128.75881463815347],
-    [17.055714221336178, -59.9846634648619]
-  ])
-  // Request permission to get location.
-  // .locate({
-  //   setView: false, // true means the map zooms to current location.
-  //   maxZoom: 8
-  // })
-
-
 
   // CUSTOMIZE
   // const world_borders_inside = L.geoJson.ajax("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json", {
@@ -198,167 +362,20 @@ function initMap(locations) {
 
   // LEGEND AND MARKERS
 
-  let legendClass = 'map-legend-item'
-  let legendHtml = '' // Create each item of legend, in HTML
-  let legendMarker = ''
-  let locationTypeHuman = '' // Used for labelling the legend, so instead of just “Water”, it will be “Place to get water”
-
   // Beginning legend item HTML with current location.
   // Use the class hide to hide by default (use that string to search elsewhere it is used)
   legendHtml += '<li class="'+ legendClass +' js-map-legend-item current-location hide">'
   + '<div class="map-legend-symbol"><img src="' + markerPathCurrent + '" alt=""></div>'
   + '<div class="map-legend-label">Current location</div></li>'
 
-  for (let key in locations) {
-    // Change marker icon depending on type of location.
-    // Set the marker path for the legend item icon.
-    switch(key) {
-      case "blm":
-        // Use the class hide to hide by default (use that string to search elsewhere it is used)
-        legendClass = 'map-legend-item js-map-legend-item blm hide ' + key
-        // legendHtml += markerPathBLM
-        legendMarker = markerPathBLM
-        locationTypeHuman = 'Some BLM'
-        svgIcon = svgBLM
-        break
-      case "places":
-        legendClass = 'map-legend-item js-map-legend-item ' + key
-        // legendHtml += markerPathPlace
-        legendMarker = markerPathPlace
-        locationTypeHuman = 'Random place'
-        svgIcon = svgPlace
-        break
-      case "potential-spots":
-        // Use the class hide to hide by default (use that string to search elsewhere it is used)
-        legendClass = 'map-legend-item js-map-legend-item hide ' + key
-        legendMarker = markerPathPotential
-        locationTypeHuman = 'Potential spots'
-        svgIcon = svgPotential
-        break
-      case "spots":
-        legendClass = 'map-legend-item js-map-legend-item ' + key
-        // legendHtml += markerPathCampspot
-        legendMarker = markerPathCampspot
-        locationTypeHuman = 'Chilling, sleeping and/or working spot'
-        svgIcon = svgSpots
-        break
-      case "starbucks":
-        legendClass = 'map-legend-item js-map-legend-item ' + key
-        // legendHtml += markerPathPlace
-        legendMarker = markerPathPlace
-        locationTypeHuman = 'Starbucks'
-        svgIcon = svgStarbucks
-        break
-      case "shower":
-        legendClass = 'map-legend-item js-map-legend-item ' + key
-        // legendHtml += markerPathWater
-        legendMarker = markerPathWater
-        locationTypeHuman = 'Shower spot'
-        svgIcon = svgShower
-        break
-      case "water":
-        legendClass = 'map-legend-item js-map-legend-item ' + key
-        // legendHtml += markerPathWater
-        legendMarker = markerPathWater
-        locationTypeHuman = 'Water for cleaning or drinking'
-        svgIcon = svgWater
-        break
-      default:
-        legendClass = 'map-legend-item js-map-legend-item ' + key
-        // legendHtml += markerPathCampspot
-        legendMarker = markerPathCampspot
-        svgIcon = svgDefault
-    } // switch
-
-
-    legendHtml += '<li class="'+ legendClass +'"><div class="map-legend-symbol"><img src="' + legendMarker
-
-    let count = 0 // For counting locations by type.
-
-    // Loop through locations and add them to the map.
-    for (let key2 in locations[key]) {
-      let location = locations[key][key2]
-
-      // Check if location should be on the map and obviously, make sure there are coordinates.
-      if (location.visible !== 0 && location.latitude && location.longitude) {
-
-        count++
-
-        let locationName = "",
-            locationNotes = "",
-            locationUrl = ""
-
-        // Create URL to Google Maps from coordinates.
-        let googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=" + location.latitude + "%2C" + location.longitude
-
-        // If there is no name, create it from coordinates.
-        if (location.name == undefined) locationName = location.latitude + ", " + location.longitude
-        else locationName = location.name
-        locationName = '<a class="location-tooltip-name" href="' + googleMapsUrl + '" target="_blank">' + locationName + '</a>'
-
-        // Check other content
-        if (location.notes !== undefined && location.notes !== "") locationNotes = '<div class="location-tooltip-notes">' + location.notes + '</div>'
-        if (location.url !== undefined && location.url !== "") locationUrl = '<a class="location-tooltip-action" target="_blank" href="' + location.url + '">' + location.url + '</a>'
-
-        // Build the content of tooltips.
-        let locationContent = '<div class="location-tooltip-content">' + locationName + locationNotes + locationUrl + '</div>'
-
-        // Place markers on the map.
-        marker = new L.marker([location.latitude, location.longitude], {icon: svgIcon})
-          .bindPopup(locationContent, { offset: L.point(0,-14) })
-          // Comment this line if using clusters
-          .addTo(map);
-
-        // Uncomment this line if using clusters
-        // markers.addLayer(marker);
-
-      } // if
-    } // for (individual places)
-
-    // End legend item with the label.
-    // legendHtml += '" alt=""></div><div class="map-legend-label">'+ locationTypeHuman +' ('+ locations[key].length +')</div></li>'
-    legendHtml += '" alt=""></div><div class="map-legend-label '+ locations[key].length +'">'+ locationTypeHuman +' ('+ count +')</div></li>'
-  } // for (first level in json)
+  parseLocations(locations)
 
   document.querySelector('#map-legend-items').innerHTML = legendHtml
 
-  // TOGGLE LEGEND AND MARKERS
-  const legendToggle = document.querySelectorAll('.js-map-legend-item')
-  for (let i = 0; i < legendToggle.length; i++) {
-    legendToggle[i].addEventListener('click', function() {
-      this.classList.toggle('hide')
-      let markers
-      if (this.classList.contains('current-location')) {
-        markers = document.querySelectorAll('.js-marker-icon.current-location')
-      }
-      else if (this.classList.contains('spots')) {
-        markers = document.querySelectorAll('.js-marker-icon.spots')
-      }
-      else if (this.classList.contains('blm')) {
-        markers = document.querySelectorAll('.js-marker-icon.blm')
-      }
-      else if (this.classList.contains('places')) {
-        markers = document.querySelectorAll('.js-marker-icon.places')
-      }
-      else if (this.classList.contains('potential-spots')) {
-        markers = document.querySelectorAll('.js-marker-icon.potential-spots')
-      }
-      else if (this.classList.contains('shower')) {
-        markers = document.querySelectorAll('.js-marker-icon.shower')
-      }
-      else if (this.classList.contains('starbucks')) {
-        markers = document.querySelectorAll('.js-marker-icon.starbucks')
-      }
-      else if (this.classList.contains('water')) {
-        markers = document.querySelectorAll('.js-marker-icon.water')
-      }
-      for (let j = 0; j < markers.length; j++) {
-        markers[j].classList.toggle('hide')
-      }
-    })
-  }
+  toggleLegendAndMarkers()
 
   // END LEGEND AND MARKERS
+
 
 
   // CURRENT LOCATION
@@ -367,7 +384,7 @@ function initMap(locations) {
     var longitude = position.coords.longitude;
     // myresult.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
     // makeMyMap(latitude, longitude);
-    // console.log("latitude: "+latitude+" and longitude: "+longitude)
+    // DEBUG && console.log("latitude: "+latitude+" and longitude: "+longitude)
 
     markerHere = new L.marker([latitude, longitude], {icon: svgCurrent})
       .bindPopup("You are here.", { offset: L.point(0,-14) })
