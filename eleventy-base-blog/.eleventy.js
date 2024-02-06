28f0ec000079309cf6c96980f2f8f535098674c9
Long Nguyen <long@longzero.com>
2024-02-05 17:03:33 -0800

    email variable

--- eleventy-base-blog/.eleventy.js
+++ eleventy-base-blog/.eleventy.js
@@ -19,6 +19,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addGlobalData("hostname", "http://localhost:8080");
   }
 
+  eleventyConfig.addNunjucksGlobal("emailAddress", "long@longzero.com");
+
   // Copy the `images` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("robots.txt");
